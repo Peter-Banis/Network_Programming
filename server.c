fbe3bbb6682084ef91d15dc5be214a96503d29c0
Klaus Cipi <kcipi2015@my.fit.edu>
2018-03-06 03:11:01 -0500

    Gossip function implemented

--- server.c
+++ server.c
@@ -31,7 +31,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+//add global variable that will hold the number of peers seen
+
 void dostuff(int); /* function prototype */
+int isKnownGossip(char*);
+
 void error(char *msg)
 {
     perror(msg);
@@ -78,19 +82,28 @@ int main(int argc, char *argv[])
      return 0; /* we never get here */
 }
 
+/*
+ KLAUS: removeNewLines will only remove the first '/n', so I suggested a solution.
+ */
 int removeNewLines(char * str) {
     int len = strlen(str);
     int state = 0; //if state is 1 we have encountered a newline and
                    //so we are going to be shifting
-    unsigned int numNewline = 0; //no way we could have a string large enough for overflow
+    unsigned int numNewline = 0;       //KLAUS: No need for this. Use state. Remove it.
     for (int i = 0; i < len; i++) {
-         if (state) {
-             str[i]=str[i+1]
+        if (state) {                   //KLAUS: remove this
+            str[i]=str[i+1];           //KLAUS: str[i]=str[i+state];
          }
-         if (str[i] == '\n') { state = 1;} //once we've encountered a \n we never stop shifting, so there is no problem
+        if (str[i] == '\n') {
+            state = 1;                 //KLAUS: state++;
+        }
+        
+        //once we've encountered a \n we never stop shifting, so there is no problem
     }
-    int newLen = len-numNewline;
+    int newLen = len-numNewline;                     //KLAUS: int newLen = len-state;
     if (str[newLen] == '\n') str[newLen] = '\0';
+                                                     //KLAUS: return smth here.
+                                                     //KLAUS: propabily return state;
 }
 /*
  * takes: destination buffer, source buffer, length of dest, src buffers
@@ -99,7 +112,7 @@ int removeNewLines(char * str) {
  */
 int bufAppend(char * dest, char * src, int destLen, int srcLen) {
     int destN = strlen(dest);
-    int srcN = strlen(source);
+    int srcN = strlen(src);
     if (destLen-destN < srcN) return -1; //not enough room
     for (int i = 0; i <= srcN; i++) { //using <= assures copying of '\0'
         dest[destN+i] = src[i];
@@ -113,15 +126,103 @@ int bufAppend(char * dest, char * src, int destLen, int srcLen) {
  *         could still have successful gossips to others even if -1 is returned
  */
 int GOSSIP(char * buf) {
-
+    char sha[126];
+    char time[64];
+    char message[1024];
+    
+    int bindex = 0, index = 0, length = strlen(buf);
+    
+    while (buf[bindex] != ':') {                        //skip GOSSIP:
+        bindex++;
+    }
+    bindex++;
+    while (buf[bindex] != ':') {                        //extract sha256 from gossip
+        sha[index] = buf[bindex];
+        index++;
+        bindex++;
+    }
+    sha[index] = '\0';
+    index = 0;
+    bindex++;
+    while (buf[bindex] != ':') {                        //extract time from gossip
+        time[index] = buf[bindex];
+        index++;
+        bindex++;
+    }
+    time[index] = '\0';
+    index = 0;
+    bindex++;
+    while (buf[bindex] != '%') {                        //extract message from gossip
+        message[index] = buf[bindex];
+        index++;
+        bindex++;
+    }
+    message[index] = '\0';
+    
+    if (isKnownGossip(message)) {
+        error("DISCARDED");
+    } else {
+        FILE * fgossip;
+        fgossip = fopen("ftest.txt", "a");               //open file to write
+        
+        fprintf(fgossip, "BEGIN\n");                     //write header
+        fprintf(fgossip, "%s\n",message);                //write message
+        fprintf(fgossip, "%s\n",time);                   //write timestamp
+        fprintf(fgossip, "%s\n",sha);                    //write sha
+        fprintf(fgossip, "END\n");                       //write footer
+        
+        if (fclose(fgossip)) { error("File not closed properly"); };  //close file
+        
+        //broadcast the message (I have not idea how :P)
+        
+        error(message);                                  //print message
+    }
 }
+/*
+ * takes a message and checks if the message exists in GOSSIP file.
+ * returns 1 if it does find the message.
+ * returns 0 if it does not find the message.
+ */
+int isKnownGossip(char* message) {
+    FILE * fgossip;
+    fgossip = fopen("ftest.txt", "r");                  //open file
+    
+    char currC;                                         //holds current char
+    int skipFlag = 0, index = 0;
+    while (fscanf(fgossip,"%c", &currC) == 1) {         //is eof?
+        if (!skipFlag) {
+            if (message[index] == '\0' && currC == '\n') {     //end of string? Found it!
+                if (fclose(fgossip)) { error("File not closed properly"); };
+                return 1;
+            } else if (currC != message[index]){        //not a match
+                skipFlag = 1;
+            } else {                                    //match. Check the next char
+                index++;
+            }
+        } else {                                         //skip the entire line
+            if (currC == '\n'){
+                index = 0;
+                skipFlag = 0;
+            }
+        }
+    }
+    if (fclose(fgossip)) { error("File not closed properly"); };
+    return 0;
+}
+
 /*
  *   adds a peer to our peer set
  *   returns 0 on successful addition
  *   returns -1 on any failure
  */
 int PEER(char * buf) {
-
+    //exctract name, ip, address
+    
+    //if (isKnownPeer())
+        //updatePeer()
+    //else
+        //addPeer()
+    
 }
 /*
  * NOTE: writes to socket directly, assumes stdin/out are mapped to socket
@@ -129,8 +230,7 @@ int PEER(char * buf) {
  * returns -1 on any error
  */
 int PEERS(char * buf) {
-    
-
+    //printAllPeers()
 }
 
 
@@ -148,10 +248,10 @@ void dostuff (int sock)
    bzero(buffer,1024); 
    char bufTemp[256]; //will be used to hold individual reads
    bzero(bufTemp, 256); 
-   while (n = read(sock, bufTemp, 255) {
+   while (n = read(sock, bufTemp, 255)) {
        if (n==-1) error("Error on reading socket");
        int r = bufAppend(buffer, bufTemp, 1024, 256);
-       if (r==-1)dwa {
+       if (r==-1) {
            //if this occurs then buffer should have some command to be executed
            //execute the commands it can and then retry bufAppend
            //this may seem  be pointless since we check for executable
@@ -165,6 +265,6 @@ void dostuff (int sock)
 
        }
    }
-   int res;
-   while(res = close(sock));
+   //int res;
+   //while(res = close(sock));
 }
