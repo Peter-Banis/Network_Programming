0be9ed55f1a352cf575b7ac641656e5b63603763
Peter-Banis <charlie.banis@gmail.com>
2018-03-22 19:01:40 -0400

    Client.java has completed functions: messageHandler, PEERS, GOSSIP, PEER Client.java has nearly completed functions: generateTimestamp(timezone portion may be a problem. If not, the function is complete) Client.java has empty functions: sendMessage, receiveResponse, displayResponse, generateHash

--- Client.java
+++ Client.java
@@ -1,24 +1,87 @@
-public class Client {
-    
+import java.io.*;
+import java.net.*;
+import java.security.MessageDigest;
+import java.text.*;
+import java.util.Date;
+import java.util.Base64;
 
+
+public class Client {
+    private Socket TCPserver;
+    private InputStream in;
+    private OutputStream out;
+    private DatagramSocket UDPserver;
+    private String initialTimestamp;
+
+
+    //We distinguish the TCP and UDP constructor by having the TCP constructor require a useless int
+    public Client(String host, int port, String initialTimestamp, String initialMessage, int distinguisher) {
+        try {
+            TCPserver = new Socket(host, port);
+            in = TCPserver.getInputStream();
+            out = TCPserver.getOutputStream();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        this.initialTimestamp = initialTimestamp;
+        if (initialMessage != null) {
+            messageHandler(initialMessage);
+        }      
+    }
     
+    public Client(String host, int port, String initialTimestamp, String initialMessage) {
+        try {
+            UDPserver = new DatagramSocket();
+            UDPserver.connect(InetAddress.getByName(host), port);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        this.initialTimestamp = initialTimestamp;
+        if (initialMessage != null) {
+            messageHandler(initialMessage);
+        }
+    }
+
+   
     private void messageHandler(String input) {
         if (input.equals("PEERS?")) {
             PEERS(input);
+        } else if (input.indexOf("PEER") != -1) {
+            PEER(input);
+        } else {
+            GOSSIP(input);
         }
     }
 
     private void GOSSIP(String s) {
         //if first message and timestamp, generateHash(message, timestamp)
-        //else generateHash(message, generateTimestamp())
+        String hash, timestamp;
+        timestamp = generateTimestamp();
+        //if an initial timestamp was given, the first message and only the first message will use it
+        if (initialTimestamp != null) {
+            timestamp = initialTimestamp;
+            //wipes condition so we only use an argument timestamp once, otherwise generate them
+            initialTimestamp = null;
+        }
+        hash = generateHash(s, timestamp);
+        //               GOSSIP:   [sha]   :     [timestamp] :  [message] %
+        String toSend = "GOSSIP:" + hash + ":" + timestamp + ":" + s + "%";
+        if (TCPserver != null) {
+
+        } else { //construct datagram packet and send it 
+ 
+        }
+        
     }
 
     private String generateTimestamp() {
-        return null; //placeholder
+        SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-dd-HH-mm-ss-SSS");
+        String timestamp = formatter.format(new Date()); timestamp +="Z"; //This is cheating until I figure out how to properly end timezone
+        return timestamp;
     }
 
     private String generateHash(String message, String timestamp) {
-        return null; //placeholder
+        return ""; //placeholder
     }
 
     private void sendMessage(String s) {
@@ -40,7 +103,7 @@ public class Client {
     }
     
     private void receiveResponse() {
-    
+        
     }
 
 
