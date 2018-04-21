169fcf00d44eb694f1fc09627cb63a9a8ac14ed0
kcipi2015 <kcipi2015@my.fit.edu>
2018-04-21 03:01:43 -0400

    added ASN1 file for LEAVE command

---
+++ Leave.h
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "BigInteger.h"
+#include "ASN1_Util.h"
+#include "ASN1Decoder.h"
+#include "ASN1Encoder.h"
+
+/*
+ Leave ::= [APPLICATION 4] EXPLICIT SEQUENCE {name UTF8String}
+ */
+static const byte TAG_8 = ASN1_Encoder::buildASN1byteType(ASN1_Encoder::CLASS_UNIVERSAL, ASN1_Encoder::PC_PRIMITIVE,ASN1_Encoder::TAG_UTF8String);
+
+static const byte TAG_4 = ASN1_Encoder::buildASN1byteType(ASN1_Encoder::CLASS_APPLICATION, ASN1_Encoder::PC_CONSTRUCTED,(byte)4);
+
+struct Leave : public ASNObjArrayable
+{
+    const char* name;
+    
+    ASN1_Encoder* getEncoder() {
+        ASN1_Encoder * r = new ASN1_Encoder();
+        r->initSequence();
+        r->addToSequence(new ASN1_Encoder(name, TAG_8));
+        r = r->setASN1TypeExplicit(TAG_4);
+        return r;
+    }
+    
+    Leave* decode(ASN1_Decoder* d) {
+        d = d->getContentExplicit();
+        name = d->getString();
+        delete d;
+        return this;
+    }
+    
+    byte getASN1Type() {
+        return TAG_4;
+    }
+    
+    Leave* instance() {
+        return new Leave();
+    }
+};
